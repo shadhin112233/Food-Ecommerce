800bd081b0d771322dfda254638c10df9a5454ba
Sharoare Shadhin <sharoareshadhin271@gmail.com>
2026-05-19 00:24:41 +0600

    Update API URL to production endpoint

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import Orders from './pages/Orders/Orders'
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-ecommerce-backendd.onrender.com"
   return (
     <div>
       <ToastContainer/>
